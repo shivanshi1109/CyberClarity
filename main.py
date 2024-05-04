95f0ddb0a9081a5eae99833f81c6c0b242792bc8
5hivanshi <shivanshisingh1109@gmail.com>
2024-05-04 20:52:24 +0530

    till video to audio

--- main.py
+++ main.py
@@ -64,3 +64,28 @@ video_url = input("Youtube Link: ")
 output_path = "./uploads"
 
 download_youtube_audio_as_mp4(video_url, output_path)
+
+# Convert to text
+import moviepy.editor as mp 
+import speech_recognition as sr 
+
+# Load the video 
+video = mp.VideoFileClip("./uploads/audio.mp4") 
+
+# Extract the audio from the video 
+audio_file = video.audio 
+audio_file.write_audiofile("./uploads/audio.wav") 
+
+# Initialize recognizer 
+r = sr.Recognizer() 
+
+# Load the audio file 
+with sr.AudioFile("./uploads/audio.wav") as source: 
+	data = r.record(source) 
+
+# Convert speech to text 
+text = r.recognize_google(data) 
+
+# Print the text 
+print("\nThe resultant text from video is: \n") 
+print(text) 
